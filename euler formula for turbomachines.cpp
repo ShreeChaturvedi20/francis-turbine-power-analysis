dc88dee1d80ab5a5a09873b163ffcf873dc79e86
ShreeChaturvedi20 <shreechaturvedi2004@gmail.com>
2025-06-24 13:07:26 +0530

    code

---
+++ euler formula for turbomachines.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+    double rho = 1000; // Density [kg/m^3]
+    double Q = 2;      // Flow rate [m^3/s]
+    double H = 10;     // Head [m]
+    double g = 9.81;   // Gravitational acceleration [m/s^2]
